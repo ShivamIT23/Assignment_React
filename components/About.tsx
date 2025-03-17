33d550966a25bbdb2f55ea72915dd2babfac5ae9
ShivamIT23 <shivam.gupta.23112003@gmail.com>
2025-03-17 22:48:34 +0530

    Added code

--- components/About.tsx
+++ components/About.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="min-h-screen mt-24 p-4 bg-black text-white">
+      <h2 className="text-3xl text-zinc-100 text-center mb-4 font-medium">
+        About Page
+        <span className="block text-blue-300 font-medium">
+          ✦ Coming soon... 
+        </span>
+      </h2>
+    </div>
+  );
 }
